18ec223b2c5c5dec3a2d9d3d9d21b86e5f2945b3
wwdfdo <wwdfdo@gmail.com>
2022-10-04 11:27:20 +0530

    added new styles to menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,18 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="w-full flex justify-center">
-      <div className=" fixed flex justify-between min-h-[80px] items-center w-[98%] mx-auto z-10  ">
+      <div className=" fixed flex justify-between min-h-[100px] items-center w-[98%] mx-auto z-10  ">
         <div className="p-2 bg-red-500 rounded-md">
           <img className="w-16 " src={logo} alt="" />
         </div>
         <ul className="flex gap-5 items-center">
           {menus.map((menu) => (
-            <li className=" font-semibold text-sm px-3 h-7 text-center rounded-md flex items-center cursor-pointer bg-gray-300 bg-opacity-40 hover:bg-opacity-80">
-              <Link to={menu.url}> {menu.name}</Link>
-            </li>
+            <Link
+              to={menu.url}
+              className="focus:underline focus:bg-opacity-80 hover:bg-opacity-80   bg-opacity-40 bg-[#d6d4d1] tracking-[.1825rem] font-medium text-[0.6rem] px-3 h-7 text-center rounded-md flex items-center cursor-pointer "
+            >
+              <li> {menu.name}</li>
+            </Link>
           ))}
         </ul>
       </div>
