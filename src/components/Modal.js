18c760ccb17e9f2d80bd9fa385eec6a561951ba8
wwdfdo <wwdfdo@gmail.com>
2022-10-11 13:51:31 +0530

    added styles to modal image

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,9 @@
 import React from "react";
 // import "./Modal.css";
-import { GrUserManager } from "react-icons/gr";
+
 import { BiFace } from "react-icons/bi";
-import { GiLoincloth } from "react-icons/gi";
-import { CgSmileMouthOpen } from "react-icons/cg";
+
 import { AiFillEye } from "react-icons/ai";
-import { GiBackgammon } from "react-icons/gi";
-import { IoIosHand } from "react-icons/io";
-import { nfts } from "../arrays/products";
 
 function Modal(props) {
   const { bgcolor, modalImg, modalSrialNo, modalobj } = props;
@@ -15,11 +11,11 @@ function Modal(props) {
   return (
     <div className="flex justify-center items-center relative">
       <div
-        className={` w-4/5 ${bgcolor} flex justify-between items-center fixed top-[10%] bottom-[10%] z-50 rounded-xl gap-20 p-10`}
+        className={` w-4/5 ${bgcolor} flex justify-between fixed top-[10%] bottom-[10%] z-50 rounded-xl gap-8`}
       >
-        <img src={modalImg} className="w-1/3 ml-10" alt="" />
-        <div className="w-2/3 flex flex-col gap-5">
-          <div className="w-full  flex items-center bg-black bg-opacity-10 py-2 px-3 gap-3 rounded-xl text-white font-semibold">
+        <img src={modalImg} className="max-w-full rounded-xl" alt="" />
+        <div className="w-2/3 flex flex-col gap-5 justify-center p-10">
+          <div className="w-full  flex items-center bg-black bg-opacity-10 py-2 px-4 gap-3 rounded-xl text-white font-semibold">
             <span className=" text-4xl opacity-30">#</span>
             <div className=" flex flex-col">
               <span className="text-sm opacity-30">AZUKI</span>
@@ -30,7 +26,7 @@ function Modal(props) {
           </div>
           <div className=" grid grid-cols-2 grid-rows-4 gap-4 text-[0.725rem] font-bold text-black">
             {modalobj.map((item) => (
-              <div className=" bg-white bg-opacity-10 rounded-lg p-2 flex items-center gap-2">
+              <div className=" bg-white bg-opacity-10 rounded-lg py-2 px-4 flex items-center gap-2">
                 <div>{item.icon}</div>
                 <div className="flex flex-col">
                   <span className="opacity-30">{item.category}</span>
@@ -39,7 +35,7 @@ function Modal(props) {
               </div>
             ))}
           </div>
-          <div className="w-full  flex justify-between items-center bg-black bg-opacity-10 p-2 gap-5 rounded-xl text-white ">
+          <div className="w-full  flex justify-between items-center bg-black bg-opacity-10 px-4 py-3 gap-5 rounded-xl text-white ">
             <div>view on :</div>
             <div className="flex gap-2">
               <div>
