23bfa27d3a115d02a9088f4b87cb6f0708dc01ec
wwdfdo <wwdfdo@gmail.com>
2022-10-25 15:43:48 +0530

    added new grid animation

--- src/components/RoadmapTwo.js
+++ src/components/RoadmapTwo.js
@@ -7,11 +7,11 @@ function RoadmapTwo() {
     <div className="w-[95%] mx-auto pt-[5rem] ">
       <div className="grid gap-3 py-10">
         <div className=" grid grid-cols-[35%_15%_28%_20%] grid-rows-[300px] gap-3">
-          <div className=" bg-black rounded-tr-2xl rounded-tl-2xl rounded-bl-2xl rounded-br-2xl relative item2 text-white">
+          <div className=" bg-black rounded-tr-2xl rounded-tl-2xl rounded-bl-2xl rounded-br-2xl relative item2 text-white overflow-hidden border-4 border-red-700 border-opacity-0 hover:border-opacity-100 animation2">
             <div className=" text-8xl font-extrabold  number2 absolute left-10 animation2">
               01
             </div>
-            <div className="flex items-center text-2xl font-semibold absolute top-[7rem] left-4 animation2 survivor-title2 ">
+            <div className="flex items-center text-2xl font-semibold absolute top-[7rem] left-4 animation2 survivor-title2  ">
               <span className="animation flex items-center font-extrabold animation survivor-arrow2 opacity-0  ">
                 &#62;
               </span>
