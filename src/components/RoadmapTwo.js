4a4847e650b7312b3b4d13d6e8b5e5be567b140d
wwdfdo <wwdfdo@gmail.com>
2022-10-25 14:06:59 +0530

    added new animation to more component

--- src/components/RoadmapTwo.js
+++ src/components/RoadmapTwo.js
@@ -1,12 +1,25 @@
 import React from "react";
+import girl from "../images/cartoon-girl.webp";
+import "./RoadmapTwo.css";
 
 function RoadmapTwo() {
   return (
     <div className="w-[95%] mx-auto pt-[5rem] ">
       <div className="grid gap-3 py-10">
         <div className=" grid grid-cols-[35%_15%_28%_20%] grid-rows-[300px] gap-3">
-          <div className=" bg-black rounded-tr-2xl rounded-tl-2xl rounded-bl-2xl rounded-br-2xl">
-            test
+          <div className=" bg-black rounded-tr-2xl rounded-tl-2xl rounded-bl-2xl rounded-br-2xl relative item2 text-white">
+            <div className=" text-8xl font-extrabold  number2 absolute left-10 animation2">
+              01
+            </div>
+            <div className="flex items-center text-2xl font-semibold absolute top-[7rem] left-4 animation2 survivor-title2 ">
+              <span className="animation flex items-center font-extrabold animation survivor-arrow2 opacity-0  ">
+                &#62;
+              </span>
+              Survivors
+            </div>
+            <div className="absolute w-3/4 survivor-girl-two animation2 -bottom-32 right-0">
+              <img src={girl} alt="" />
+            </div>
           </div>
           <div className=" bg-blue-500 rounded-tr-2xl rounded-tl-2xl rounded-bl-2xl rounded-br-2xl ">
             test
