2599795934ce299fec7da20145bc96e07b31756d
wwdfdo <wwdfdo@gmail.com>
2023-02-04 13:15:06 +0530

    added maincowgirl to homepage v4

--- src/components/VideoBg.js
+++ src/components/VideoBg.js
@@ -12,7 +12,7 @@ function VideoBg() {
         <source src={Branz} type="video/mp4" />
       </video> */}
       <img
-        className="w-[540px] absolute top-[12%] right-[13%] "
+        className="w-[560px] absolute top-[10%] right-[13%] "
         src={Cowgirl}
         alt=""
       />
