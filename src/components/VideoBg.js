e6b9e5eb716f4813be84b99b98ce22827c4711ae
wwdfdo <wwdfdo@gmail.com>
2023-02-06 14:51:20 +0530

    added new styles for homepage v1

--- src/components/VideoBg.js
+++ src/components/VideoBg.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Branz from "../images/beanz2.mp4";
 import Rock from "../images/rock.png";
-import Cowgirl from "../images/maincowgirl.png";
+import Cowgirl from "../images/maincowgirl1.png";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import "./VideoBg.css";
 
 function VideoBg() {
   return (
-    <div className="relative  w-full h-screen azuki-home-bg">
+    <div className="relative  w-full h-screen azuki-home-bg flex items-center">
       {/* <video loop autoPlay muted className="w-full h-screen object-cover">
         <source src={Branz} type="video/mp4" />
       </video> */}
-      <div className=" absolute w-full flex justify-center bottom-0 pb-24  ">
+      <div className="  w-full flex justify-center gap-[10%]  bottom-0 pb-32 xl-pb-40  ">
         <div className="  flex flex-col gap-5 justify-center ">
-          <h2 className="text-[3.8rem] font-bold ">
+          <h2 className="text-[3.8rem] xl-text-[4rem] font-bold ">
             Embark on a Journey with <br /> John Marston in <br />
             The Outlaw's Last Ride
           </h2>
@@ -22,7 +22,7 @@ function VideoBg() {
             Your Former Gang and Make a Final Stand
           </h2>
         </div>
-        <img className="w-[560px]   " src={Cowgirl} alt="" />
+        <img className="w-[22%] xl-w-[28%]" src={Cowgirl} alt="" />
       </div>
       <div className="flex absolute  bottom-5 right-5 w-1/2 bg-red-800 h-24 z-20  items-center justify-between rounded-md shadow-lg">
         <img src={Rock} className="w-20 absolute left-2 bottom-1" alt="" />
