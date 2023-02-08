82b678adf35c1df76d47d5897bf7e09477e2bf31
wwdfdo <wwdfdo@gmail.com>
2023-02-08 16:51:49 +0530

    added new bg

--- src/components/Worldbg.js
+++ src/components/Worldbg.js
@@ -1,12 +1,15 @@
 import React from "react";
-import worldbg from "../images/world1.mp4";
+// import worldbg from "../images/world1.mp4";
+import horsebg from "../images/horsebg.png";
 
 function Worldbg() {
   return (
     <div className="relative">
-      <video loop autoPlay muted className="w-full h-screen object-cover">
+      {/* <video loop autoPlay muted className="w-full h-screen object-cover">
         <source src={worldbg} type="video/mp4" />
-      </video>
+      </video> */}
+
+      <img src={horsebg} className="w-full h-screen object-cover" alt="" />
     </div>
   );
 }
