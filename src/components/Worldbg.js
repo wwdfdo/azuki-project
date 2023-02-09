d238c5f22f41f4c0ee88c56058209aaa96448f9b
wwdfdo <wwdfdo@gmail.com>
2023-02-09 09:50:43 +0530

    added new homepage heading style

--- src/components/Worldbg.js
+++ src/components/Worldbg.js
@@ -4,14 +4,14 @@ import React from "react";
 
 function Worldbg() {
   return (
-    <div className="relative bg-horse-bg w-full h-screen bg-cover">
+    <div className="relative bg-horse-bg w-full h-screen bg-cover flex  items-center xl-items-center max-w-[3000px]">
       {/* <video loop autoPlay muted className="w-full h-screen object-cover">
         <source src={worldbg} type="video/mp4" />
       </video> */}
 
-      <div className="  w-[1/3] mx-auto pl-[20%] flex pt-[10%] ">
+      <div className="  w-full flex pl-[20%]     ">
         <div className="  flex flex-col gap-5 justify-start ">
-          <h2 className="text-[4.3rem] xl-text-[5.2rem]  ">
+          <h2 className="text-[4.4rem] xl-text-[4.6rem]  ">
             Embark on a Journey with <br /> Abigail Marston in <br />
             The Outlaw's Last Ride
           </h2>
