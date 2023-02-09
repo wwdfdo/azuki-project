80b039d0e4f599bc27e7c4098fbab70130b98b3e
wwdfdo <wwdfdo@gmail.com>
2023-02-09 13:24:17 +0530

    added new header view

---
+++ src/arrays/navButtons.js
@@ -0,0 +1,19 @@
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { BsGlobe } from "react-icons/bs";
+
+export const navButtons = [
+  {
+    id: 1,
+    name: <BsGlobe size={16} />,
+    style: "border-solid border-2 border-white p-2  rounded-[50%]",
+
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Play",
+    style: "border-solid border-2 border-white px-4 rounded-2xl",
+    url: "/",
+  },
+];
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Header from "./Header";
+import Header2 from "./Header2";
+// import Header from "./Header";
 import "./Layout.css";
 
 function Layout(props) {
   return (
     <div className="bg-black text-white font-normal">
-      <Header />
+      {/* <Header /> */}
+      <Header2 />
       <main>{props.children}</main>
     </div>
   );
